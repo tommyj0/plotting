944ec81c3333b1475cdb1186000b3bebed034f84
tommyj0 <thomasfj17@hotmail.com>
2023-05-14 17:40:41 +0100

    moved defs to .h

--- src/plotting.c
+++ src/plotting.c
@@ -12,42 +12,8 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#include "plotting.h"
 #include "font.h"
-
-// DEFINITIONS
-// image resolution
-#define WIDTH 1000
-#define HEIGHT 1000
-#define BORDER 100
-#define BORDER_AREA HEIGHT - 2*BORDER
-#define PLOT_BORDER 20
-#define PLOT_WIDTH WIDTH - 2*PLOT_BORDER - 2*BORDER
-#define PLOT_HEIGHT HEIGHT - 2*PLOT_BORDER - 2*BORDER
-#define CHANNEL_NUM 3
-#define PI 3.14
-
-// defaults
-#define DEFAULT_FILE_PATH "plot.png"
-#define DEFAULT_TITLE "plot title"
-#define DEFAULT_X_LABEL "x-axis"
-#define DEFAULT_Y_LABEL "y-axis"
-#define DOT_SIZE 3
-#define GRID_DENSITY 10
-
-// colours in hex 
-// 0xAABBGGRR
-// standard is RR GG BB 
-#define COLOR_WHITE 0xFFFFFFFF
-#define COLOR_BLACK 0xFF000000
-#define COLOR_RED   0xFF0000FF
-#define COLOR_GREEN 0xFF00FF00
-#define COLOR_BLUE  0xFFFF0000
-#define COLOR_PURPLE 0xFFFF00AF
-#define COLOR_GREY  0xFFA0A0A0
-#define COLOR_DARKGREY 0xFF505050
-
-// useless typedefs
-typedef uint32_t Colour32;
 
 // globals
 static Colour32 image[HEIGHT][WIDTH]; // define image size
